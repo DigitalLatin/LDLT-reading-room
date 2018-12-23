a2e0c2f760f9314e036cf810c3abb854af9f34cb
Samuel J. Huskey <samueljhuskey@gmail.com>
2018-12-23 15:25:03 -0600

    More fiddling.

--- library/urls.py
+++ library/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.splash, name='splash'),
-    url(r'^/library/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^texts/[^/]+$', views.add_slash),
     url(r'^texts/([^/]+)/$', views.edition),
     url(r'^texts/([^/]+)/([0-9a-zA-Z]+)$', views.edition),
