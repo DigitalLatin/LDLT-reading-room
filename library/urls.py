f610855107fff44eeccb2579894f9982283b961b
Samuel J. Huskey <samueljhuskey@gmail.com>
2018-12-23 15:09:13 -0600

    Adding a slash in library/urls.py

--- library/urls.py
+++ library/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='splash'),
-    url(r'^library/$', views.index, name='index'),
+    url(r'^/library/$', views.index, name='index'),
     url(r'^texts/[^/]+$', views.add_slash),
     url(r'^texts/([^/]+)/$', views.edition),
     url(r'^texts/([^/]+)/([0-9a-zA-Z]+)$', views.edition),
