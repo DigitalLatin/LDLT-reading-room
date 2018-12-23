0676929b1a82eb87892238e6324870fdc5aa5d62
Samuel J. Huskey <samueljhuskey@gmail.com>
2018-12-23 14:55:47 -0600

    Trying a reconfiguration of the urls.py files in LDLT and library.

--- LDLT/urls.py
+++ LDLT/urls.py
@@ -12,18 +12,14 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-    
-    Old rule: url(r'^$', RedirectView.as_view(url='/library/', permanent=True)),
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-#from django.views.generic import RedirectView
-import library
+from django.views.generic import RedirectView
 
 urlpatterns = [
-        url(r'^$', library.urls),
-#    url(r'^$', RedirectView.as_view(url='/library/', permanent=True)),
-#        #url(r'^$', views.splash, name='splash', include('library.urls')),
-#    url(r'^library/', include('library.urls')),
+    #url(r'^$', RedirectView.as_view(url='/library/', permanent=True)),
+  #url(r'^$', views.splash, name='splash', include('library.urls')),
+    url(r'^$', include('library.urls')),
     url(r'^admin/', admin.site.urls),
 ]
